b74a14ff852d426d1deb7be821e75e88fa523c45
Nathan Cox <nathan@flyingmonkey.co.nz>
2017-10-06 22:45:04 +1300

    JS bug fixing

--- javascript/betteranchors.js
+++ javascript/betteranchors.js
@@ -2,40 +2,42 @@
   $.entwine('ss', function ($) {
 
     $('form.htmleditorfield-linkform').entwine({
-        /**
-         * This fetches the non-content anchors from the textarea's data-anchors attribute and
-         * inserts them in to the anchors dropdown
-         */
-        getAnchors: function() {
-            var dfdAnchors = this._super()
+      /**
+       * This fetches the non-content anchors from the textarea's data-anchors attribute and
+       * inserts them in to the anchors dropdown
+       */
+      getAnchors: function() {
+        var dfdAnchors = this._super()
 
-            var extraAnchors = this.getElement().data('anchors')
+        var extraAnchors = this.getElement().data('anchors')
 
-            var insertBefore = []
-            var insertAfter = extraAnchors
+        if (extraAnchors !== undefined) {
+          var insertBefore = []
+          var insertAfter = extraAnchors
 
-            if (extraAnchors['before']) {
-                insertBefore = extraAnchors['before']
-            }
+          if (extraAnchors['before'] !== undefined) {
+              insertBefore = extraAnchors['before']
+          }
 
-            if (extraAnchors['after']) {
-                insertAfter = extraAnchors['after']
-            }
+          if (extraAnchors['after'] !== undefined) {
+              insertAfter = extraAnchors['after']
+          }
 
-            dfdAnchors.then(function(anchors) {
-                for (var i = insertBefore.length - 1; i >= 0; i--) {
-                    anchors.unshift(insertBefore[i]);
-                }
+          dfdAnchors.then(function(anchors) {
+              for (var i = insertBefore.length - 1; i >= 0; i--) {
+                  anchors.unshift(insertBefore[i])
+              }
 
-                for (var i = 0; i < insertAfter.length; i++) {
-                    anchors.push(insertAfter[i]);
-                }
-            })
+              for (var i = 0; i < insertAfter.length; i++) {
+                  anchors.push(insertAfter[i])
+              }
+          })
 
-            return dfdAnchors
         }
-
+        return dfdAnchors
+      }
     })
 
   })
 })(jQuery)
+
